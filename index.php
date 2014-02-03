8355b9bf53c0ca9a8804532e355785308a8df285
futoase <futoase@gmail.com>
2014-02-03 10:22:36 +0900

    Modified: Update index.php

--- index.php
+++ index.php
@@ -1,116 +1,10 @@
-<!DOCTYPE html>
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-
-    <!-- Run in full-screen mode. -->
-    <meta name="apple-mobile-web-app-capable" content="yes">
-
-    <!-- Make the status bar black with white text. -->
-    <meta name="apple-mobile-web-app-status-bar-style" content="black">
-
-    <!-- Customize home screen title. -->
-    <meta name="apple-mobile-web-app-title" content="Web App">
-
-    <!-- Disable phone number detection. -->
-    <meta name="format-detection" content="telephone=no">
-
-    <!-- Set viewport. -->
-    <meta name="viewport" content="initial-scale=1">
-
-    <!-- Prevent text size adjustment on orientation change. -->
-    <style>html { -webkit-text-size-adjust: 100%; }</style>
-
-
-    <!-- Icons -->
-
-    <!-- iOS 7 iPad (retina) -->
-    <link href="/static/images/apple-touch-icon-152x152.png"
-          sizes="152x152"
-          rel="apple-touch-icon">
-
-    <!-- iOS 6 iPad (retina) -->
-    <link href="/static/images/apple-touch-icon-144x144.png"
-          sizes="144x144"
-          rel="apple-touch-icon">
-
-    <!-- iOS 7 iPhone (retina) -->
-    <link href="/static/images/apple-touch-icon-120x120.png"
-          sizes="120x120"
-          rel="apple-touch-icon">
-
-    <!-- iOS 6 iPhone (retina) -->
-    <link href="/static/images/apple-touch-icon-114x114.png"
-          sizes="114x114"
-          rel="apple-touch-icon">
-
-    <!-- iOS 7 iPad -->
-    <link href="/static/images/apple-touch-icon-76x76.png"
-          sizes="76x76"
-          rel="apple-touch-icon">
-
-    <!-- iOS 6 iPad -->
-    <link href="/static/images/apple-touch-icon-72x72.png"
-          sizes="72x72"
-          rel="apple-touch-icon">
-
-    <!-- iOS 6 iPhone -->
-    <link href="/static/images/apple-touch-icon-57x57.png"
-          sizes="57x57"
-          rel="apple-touch-icon">
-
-    <!-- Startup images -->
-
-    <!-- iOS 6 & 7 iPad (retina, portrait) -->
-    <link href="/static/images/apple-touch-startup-image-1536x2008.png"
-          media="(device-width: 768px) and (device-height: 1024px)
-                 and (orientation: portrait)
-                 and (-webkit-device-pixel-ratio: 2)"
-          rel="apple-touch-startup-image">
-
-    <!-- iOS 6 & 7 iPad (retina, landscape) -->
-    <link href="/static/images/apple-touch-startup-image-1496x2048.png"
-          media="(device-width: 768px) and (device-height: 1024px)
-                 and (orientation: landscape)
-                 and (-webkit-device-pixel-ratio: 2)"
-          rel="apple-touch-startup-image">
-
-    <!-- iOS 6 iPad (portrait) -->
-    <link href="/static/images/apple-touch-startup-image-768x1004.png"
-          media="(device-width: 768px) and (device-height: 1024px)
-                 and (orientation: portrait)
-                 and (-webkit-device-pixel-ratio: 1)"
-          rel="apple-touch-startup-image">
-
-    <!-- iOS 6 iPad (landscape) -->
-    <link href="/static/images/apple-touch-startup-image-748x1024.png"
-          media="(device-width: 768px) and (device-height: 1024px)
-                 and (orientation: landscape)
-                 and (-webkit-device-pixel-ratio: 1)"
-          rel="apple-touch-startup-image">
-
-    <!-- iOS 6 & 7 iPhone 5 -->
-    <link href="/static/images/apple-touch-startup-image-640x1096.png"
-          media="(device-width: 320px) and (device-height: 568px)
-                 and (-webkit-device-pixel-ratio: 2)"
-          rel="apple-touch-startup-image">
-
-    <!-- iOS 6 & 7 iPhone (retina) -->
-    <link href="/static/images/apple-touch-startup-image-640x920.png"
-          media="(device-width: 320px) and (device-height: 480px)
-                 and (-webkit-device-pixel-ratio: 2)"
-          rel="apple-touch-startup-image">
-
-    <!-- iOS 6 iPhone -->
-    <link href="/static/images/apple-touch-startup-image-320x460.png"
-          media="(device-width: 320px) and (device-height: 480px)
-                 and (-webkit-device-pixel-ratio: 1)"
-          rel="apple-touch-startup-image">
-
+    <meta name="viewport" content="target-densitydpi=device-dpi, width=device-width, user-scalable=no,initial-scale=1.0,maximum-scale=1.0, minimum-scale=1.0" />
 
     <title>MAP</title>
-    <link href="./js/button.css" rel="stylesheet" media="all" type="text/css" />
+    <link href="js/button.css" rel="stylesheet" media="all" type="text/css" />
     <link href="js/map_style.css" rel="stylesheet" media="all" type="text/css" />
 
     <script type="text/javascript" src="js/jquery-1.6.4.min.js"></script>
@@ -122,29 +16,35 @@
 </head>
 <body>
 <div id="header">
-<a href="javascript:void(0);" onclick="show_float_panel('search')" class="btn">検　索</a>
+    <ul>
+        <li class="distince">
+            <span id="move_area_distince"></span>
+        </li>
+        <li class="status">
+            <select id="move_area_status">
+                <option value="open">未貼付の物</option>
+                <option value="close">貼付完了</option>
+            </select>
+        </li>
+    </ul>
+<a href="javascript:void(0);" onclick="show_float_panel('search')" class="btn">一括選択</a>
 <!--<a href="javascript:void(0);" onclick="show_info()" class="btn ">吹出し表示</a>-->
 <a href="javascript:void(0);" onclick="move_loc()" class="btn">現在地</a>
-<a href="javascript:void(0);" onclick="show_float_panel('bookmark')" class="btn">マーク</a>
+<a href="javascript:void(0);" onclick="show_float_panel('bookmark')" class="btn">★マークリスト</a>
+<a href="javascript:void(0);" onclick="show_float_panel('info')" class="btn">説 明</a>
 </div>
 	<div id="float_panel">
         <a href="javascript:void(0);" onclick="hide_float_panel()" class="btn close">閉じる</a>
         <div id="search">
             <ul>
-                <li> <span class="label">行政区</span>
-                    <span id="move_area_distince"></span>
-                </li>
-                <li><span class="label">住所で検索</span>
+                <!--todo::「住所で検索」は廃止。（住所検索しても、緯度経度から掲示場所を取得するAPIが無い為、表示しても意味が無い）-->
+                <!--<li><span class="label">住所で検索</span>
                     <input name="move_area_address" class="input_text_slm" type="text" id="move_area_address" value=""/>
                     <a href="javascript:void(0);" onclick="move_area_address()" class="btn ">移動</a>
-                </li>
+                </li>-->
                 <li>
-                    <label>ステータス
-                        <select id="move_area_status">
-                            <option value="open">未貼付け</option>
-                            <option value="close">終了</option>
-                        </select>
-                    </label>
+                    <div id="area_list"></div>
+
                 </li>
             </ul>
         </div>
@@ -153,12 +53,35 @@
             <hr/>
             <a href="javascript:void(0);" onclick="clear_book_mark()" class="btn center">クリアー</a>
         </div>
+        <div id="info">
+            <div id="info_area">
+            <p>使い方</p>
+                <h5>行政区を選択</h5>
+                <p>表示したい掲示板のある区を選択すると、その区の掲示板の位置が表示されます。<br/>表示出来るのは1つの区だけです。<br/>
+                    複数の行政区を、まとめて表示したい場合は「一括選択」を選んで下さい。
+                </p>
+                <h5>ステータス</h5>
+                <p>「未貼付の物」と「貼付完了」の掲示板の表示を切り替えます<br/>通常は「未貼付の物」でOKです。</p>
+                <h5>一括選択</h5>
+                <p>複数の行政区を、まとめて表示したい場合に使用します。<br/>表示したい区をチェックして、リスト下部の「表示」を押すと、表示されます。<br/>
+                    あまり多いと表示に時間がかかったり、スマホの場合はフリーズするかもです。<br/>
+                    5区以内を推奨します。
+                </p>
+                <h5>現在地</h5>
+                <p>現在地に移動します。GPSをオンにして下さい。</p>
+                <h5>マークリスト</h5>
+                   <p>地図上に表示された、掲示板の位置をタップすると、住所等が書かれた吹き出しが表示されます。<br/>
+                       吹き出し下の「マーク」ボタンをタップすると、その掲示板が緑色でマークされます。<br/>
+                       マークした掲示板は「マークリスト」で一覧表示されます。<br/>
+                       使い道は現場でポスターを貼った時にその場でマークして、作業終了後に「マークリスト」を見て、一括で報告するって感じです。
+                </p>
+            </div>
+
+        </div>
     </div>
 	<div id="map_wrap" >
 	<div id="map_canvas" ></div>
 	</div>
-
-
 <div id="load_lock"><img src="js/loading.gif" />検索中</div>
 </body>
 </html>
